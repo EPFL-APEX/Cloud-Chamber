1fa0d9a2d26f5b2166123b99a445418ef2abe598
th0my67 <thomy672005@gmail.com>
2026-07-14 17:16:39 +0200

    Add cooling phase logic with probing plan and state reaction methods

--- src/logic/cooling.rs
+++ src/logic/cooling.rs
@@ -1,5 +1,7 @@
 //! All of the logic that goes into cooling the chamber is implemented here
 
+use crate::{logic::probing::{MeasurementHistory, ProbingPlan}, shared::data::SystemTask};
+
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum CoolingPhase {
@@ -9,4 +11,28 @@ pub enum CoolingPhase {
     SaturatingAirWithIpa,
     HighVoltage,
     FinalCheckBeforeStabilising,
+}
+
+impl CoolingPhase {
+    pub fn create_probing_plan(&self, prob_hist: &MeasurementHistory) -> ProbingPlan {
+        match self {
+            Self::SensorCheck => todo!(),
+            Self::PreCoolingThePlate => todo!(),
+            Self::StartingIpaCirculation => todo!(),
+            Self::SaturatingAirWithIpa => todo!(),
+            Self::HighVoltage => todo!(),
+            Self::FinalCheckBeforeStabilising => todo!(),
+        }
+    }
+
+    pub fn react_to(self, current_state: &MeasurementHistory) -> SystemTask {
+        match self {
+            Self::SensorCheck => todo!(),
+            Self::PreCoolingThePlate => todo!(),
+            Self::StartingIpaCirculation => todo!(),
+            Self::SaturatingAirWithIpa => todo!(),
+            Self::HighVoltage => todo!(),
+            Self::FinalCheckBeforeStabilising => todo!(),
+        }
+    }
 }
