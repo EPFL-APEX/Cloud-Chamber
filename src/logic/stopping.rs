d7e126e8a7d75d9633dacc5464047c0295e13a94
th0my67 <thomy672005@gmail.com>
2026-07-14 17:17:13 +0200

    Add StoppingPhase enum and create_probing_plan method stub

--- src/logic/stopping.rs
+++ src/logic/stopping.rs
@@ -1,4 +1,12 @@
+use crate::logic::probing::{MeasurementHistory, ProbingPlan};
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum StoppingPhase {
     Todo
+}
+
+impl StoppingPhase {
+    pub fn create_probing_plan(&self, prob_hist: &MeasurementHistory) -> ProbingPlan {
+        todo!()
+    }
 }
