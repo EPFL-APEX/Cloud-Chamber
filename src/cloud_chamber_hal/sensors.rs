830817932f5ac169de1a793989a67b40c25721d2
th0my67 <thomy672005@gmail.com>
2026-07-14 17:18:17 +0200

    Refactor sensor traits to implement a common Sensor interface and enhance type safety, still WIP

--- src/cloud_chamber_hal/sensors.rs
+++ src/cloud_chamber_hal/sensors.rs
@@ -8,9 +8,20 @@
 //! on peut substituer n'importe quelle implémentation sans modifier
 //! `SecurityLoop`.
 
+use core::fmt::Debug;
+
+use crate::{
+    config::{NUMBER_OF_PRESSURE_SENSOR, NUMBER_OF_TEMP_SENSOR, NUMBER_OF_VOLTMETER},
+    shared::data::{PressureReading, TemperatureReading, VoltsReading}
+};
+
+pub trait Sensor<T> {
+    type Error: Debug;
+    fn read(&mut self) -> Result<T, Self::Error>;
+}
+
 /// Capteur de température retournant des degrés Celsius.
-pub trait TemperatureSensor {
-    type Error;
+pub trait TemperatureSensor : Sensor<TemperatureReading> {
     /// Déclenche une conversion (peut être asynchrone sur certains capteurs).
     fn start_measurement(&mut self) -> Result<(), Self::Error>;
     /// Lit la dernière température convertie, en °C.
@@ -18,26 +29,34 @@ pub trait TemperatureSensor {
 }
 
 /// Capteur de tension retournant des Volts.
-pub trait VoltageSensor {
-    type Error;
+pub trait VoltageSensor : Sensor<VoltsReading> {
     fn read_voltage(&mut self) -> Result<f32, Self::Error>;
 }
 
 /// Capteur de courant retournant des Ampères.
-pub trait CurrentSensor {
-    type Error;
+pub trait CurrentSensor : Sensor<T> {
     fn read_amperes(&mut self) -> Result<f32, Self::Error>;
 }
 
 /// Capteur de pression retournant des pascal
-pub trait PressureSensor {
-    type Error;
+pub trait PressureSensor : Sensor<PressureReading> {
     fn read_pascal(&mut self) -> Result<f32, Self::Error>;
 }
 
 /// Capteur de fermeture (contact sec) retournant un booléen.
-pub trait ClosureSensor {
-    type Error;
+pub trait ClosureSensor : Sensor<T> {
     /// Retourne `true` si la chambre est physiquement fermée.
     fn is_closed(&mut self) -> Result<bool, Self::Error>;
+}
+
+pub struct Sensors<T: TemperatureSensor, P: PressureSensor, V: VoltageSensor> {
+    pub temperature_sensors: [T; NUMBER_OF_TEMP_SENSOR],
+    pub pressure_sensors: [P; NUMBER_OF_PRESSURE_SENSOR],
+    pub voltage_sensors: [V; NUMBER_OF_VOLTMETER],
+}
+
+impl<T: TemperatureSensor, P: PressureSensor, V: VoltageSensor> Sensors<T, P, V> {
+    pub fn new() -> Self {
+        todo!()
+    }
 }
